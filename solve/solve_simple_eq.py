b7d618459abe5894606b19638a049b1bdfa0b992
Emma Secrest <esecrest908@gmail.com>
2024-01-04 17:01:54 -0500

    adding init file to simplify

--- solve/solve_simple_eq.py
+++ solve/solve_simple_eq.py
@@ -1,2 +1,18 @@
+from sympy import Symbol, solve
+import sys
+# sys.path.append('../simplify')
+# from simplify.simplify_expression import simplify_polynomials
+from simplify.simplify_expression import simplify_polynomials
+
 def solve_simple_eq(equation):
-    pass;
+    #simplifying the equation
+    #splitting the equation into two parts
+     parts = equation.split('=')
+     before_equal = parts[0]
+     after_equal = parts[1]
+     
+     print ("before_equal: ", before_equal)
+     print ("after_equal: ", after_equal)   
+     
+test_eq = "x+1=2"
+print(solve_simple_eq(test_eq))
