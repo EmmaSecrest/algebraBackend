8daac56d595d6b199bcf17903c5944564c07a062
Emma Secrest <esecrest908@gmail.com>
2024-06-20 19:30:59 -0400

    got my helper functions set up nothing is broken yet

--- solve/solve_higher_order.py
+++ solve/solve_higher_order.py
@@ -1,4 +1,12 @@
 from sympy import symbols, degree, Eq, solve, sympify
+import math
+from solve.solve_quadratic import (
+    determine_coefficients,
+    splitting_terms,
+    find_symbol,
+    solve_quad_no_factor,
+    solve_quad_factor
+    )
 
 def array_factors_coefficient_list(x):
     results = set()
@@ -15,6 +23,25 @@ def find_degree(equation, symbol):
     highest_degree = degree(left_side, gen=s)
     
     return highest_degree
+
+def is_quadratic_factorable(equation):
+    symbol = find_symbol(equation)
+    terms = splitting_terms(equation)
+    a, b, c = determine_coefficients(terms, symbol)
+    determinate = b**2 - 4*a*c
+    
+    if determinate < 0:
+        return False
+    if math.sqrt(determinate) % 1 == 0:
+        return True
+
+def solve_quad_switch(equation):
+    is_factorable = is_quadratic_factorable(equation)
+    if is_factorable:
+        return solve_quad_factor(equation)
+    else:
+        return solve_quad_no_factor(equation)
+
 
 
 def solve_synthetic_division(equation):
--- tests/test_solve_higher_order.py
+++ tests/test_solve_higher_order.py
@@ -1,13 +1,27 @@
 from unittest import TestCase, main
+from solve.solve_higher_order import (
+    solve_synthetic_division,
+    array_factors_coefficient_list,
+    find_degree,
+    is_quadratic_factorable,
+    solve_quad_switch
+    )
 
-from solve.solve_higher_order import (solve_synthetic_division,array_factors_coefficient_list,find_degree)
+# class TestDetermineSyntheticDivision(TestCase):
+#     def test_synthetic_division(self):
+#         self.assertEqual(solve_synthetic_division("x**3 + 2*x**2 - 5*x - 6 = 0"), ["To get the possible zeros take the factors of the constant and then divide them by the factors of the leading coefficient in this case we have (-1,1,-2,2,-3,3,-6,6) then use long division or synthetic division to divide them.",("x+1","X**2 + x - 6"), ("x-2","x-3"),['x=-1', 'x=2', 'x=3']])
+#         self.assertEqual(solve_synthetic_division("x**3 - 3*x**2 - 4*x + 12 = 0"), ["To get the possible zeros take the factors of the constant and then divide them by the factors of the leading coefficient in this case we have (-1,1,-2,2,-3,3,-4,4,-6,6,-12,12) then use long division or synthetic division to divide them.",("x-3","x**2 - 4"), "(x+2)(x-2)",['x=3', 'x=-2', 'x=2']])
+#         self.assertEqual(solve_synthetic_division("x**5 + x**4 - 15*x**3 - 25*x**2 + 14*x + 24 = 0"), ["To get the possible zeros take the factors of the constant and then divide them by the factors of the leading coefficient in this case we have (-1,1,-2,2,-3,3,-4,4,-6,6,-8,8,-12,12,-24,24) then use long division or synthetic division to divide them.",
 
-class TestDetermineSyntheticDivision(TestCase):
-    def test_synthetic_division(self):
-        self.assertEqual(solve_synthetic_division("x**3 + 2*x**2 - 5*x - 6 = 0"), ["To get the possible zeros take the factors of the constant and then divide them by the factors of the leading coefficient in this case we have (-1,1,-2,2,-3,3,-6,6) then use long division or synthetic division to divide them.",("x+1","X**2 + x - 6"), ("x-2","x-3"),['x=-1', 'x=2', 'x=3']])
-        self.assertEqual(solve_synthetic_division("x**3 - 3*x**2 - 4*x + 12 = 0"), ["To get the possible zeros take the factors of the constant and then divide them by the factors of the leading coefficient in this case we have (-1,1,-2,2,-3,3,-4,4,-6,6,-12,12) then use long division or synthetic division to divide them.",("x-3","x**2 - 4"), "(x+2)(x-2)",['x=3', 'x=-2', 'x=2']])
-        self.assertEqual(solve_synthetic_division("x**5 + x**4 - 15*x**3 - 25*x**2 + 14*x + 24 = 0"), ["To get the possible zeros take the factors of the constant and then divide them by the factors of the leading coefficient in this case we have (-1,1,-2,2,-3,3,-4,4,-6,6,-8,8,-12,12,-24,24) then use long division or synthetic division to divide them.",
-                                                                                                   ("x + 1", "x**4 - 15*x**2 - 10*x + 24"),("x + 2","x**3 - 2*x - 11x + 12"),("x+3", "x**2 - 5*x + 4"),"(x - 4)(x - 1)" ["x=-1", "x=-2", "x=-3", "x=4" ,"x=1" ]])
+class TestFactorableQuadratic(TestCase):
+    def test_is_quadratic_factorable(self):
+        self.assertEqual(is_quadratic_factorable("x**2 + 2*x + 1 = 0"), True)
+        self.assertEqual(is_quadratic_factorable("x**2 + 2*x + 2 = 0"), False)
+
+class TestSolveQuadraticSwitch(TestCase):
+    def test_solve_quad_switch(self):
+        self.assertEqual(solve_quad_switch("x**2 + 3*x + 2 = 0"), ['(x + 2)(x + 1) = 0', ['x = -2', 'x = -1']])
+        self.assertEqual(solve_quad_switch("x**2 + 2*x + 2 = 0"),["Use quadratic equation with a = 1, b = 2 and c = 2", 'No real solutions'])
 
 class TestArrayFactorsCoefficientList(TestCase):
     def test_array_factors_coefficient_list(self):
